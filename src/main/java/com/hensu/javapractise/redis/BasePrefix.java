96cae5f32a899a45cd1f678390dd7eb2329069a0
suh <2220388107@qq.com>
2019-08-26 17:25:09 +0800

    edit redis

---
+++ src/main/java/com/hensu/javapractise/redis/BasePrefix.java
@@ -0,0 +1,32 @@
+package com.hensu.javapractise.redis;
+
+/**
+ * @Author: sh
+ * @Description: KeyPrefix
+ * @Date: 17:02 2019/8/23
+ */
+public abstract class BasePrefix implements KeyPrefix{
+	
+	private int expireSeconds;
+	
+	private String prefix;
+	
+	public BasePrefix(String prefix) {//0代表永不过期
+		this(0, prefix);
+	}
+	
+	public BasePrefix( int expireSeconds, String prefix) {
+		this.expireSeconds = expireSeconds;
+		this.prefix = prefix;
+	}
+	
+	public int expireSeconds() {//默认0代表永不过期
+		return expireSeconds;
+	}
+
+	public String getPrefix() {
+		String className = getClass().getSimpleName();
+		return className+":" + prefix;
+	}
+
+}
