76ff2eaf2d119ecd893b56100f68c59ae66acd41
suh <2220388107@qq.com>
2019-08-26 13:48:11 +0800

    edit druid config

--- src/main/java/com/hensu/javapractise/config/DruidCfg.java
+++ src/main/java/com/hensu/javapractise/config/DruidCfg.java
@@ -41,8 +41,7 @@ public class DruidCfg {
         initParams.put("loginUsername","admin");
         initParams.put("loginPassword","123456");
         initParams.put("allow","");//默认就是允许所有访问
-        initParams.put("deny","192.168.15.21");
-
+        //initParams.put("deny","192.168.15.21");
         bean.setInitParameters(initParams);
         return bean;
     }
@@ -53,14 +52,10 @@ public class DruidCfg {
     public FilterRegistrationBean webStatFilter(){
         FilterRegistrationBean bean = new FilterRegistrationBean();
         bean.setFilter(new WebStatFilter());
-
         Map<String,String> initParams = new HashMap<>();
         initParams.put("exclusions","*.js,*.css,/druid/*");
-
         bean.setInitParameters(initParams);
-
         bean.setUrlPatterns(Arrays.asList("/*"));
-
         return  bean;
     }
 }
